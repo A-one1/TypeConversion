0df4350c77ff7c79eb9ed5dd2f03771d68a64d1e
Aone Shrestha <69296264+A-one1@users.noreply.github.com>
2022-04-25 15:19:20 -0600

    Test File

--- type-conversion-A-one1-master/src/TestAverage.java
+++ type-conversion-A-one1-master/src/TestAverage.java
@@ -22,7 +22,6 @@ public class TestAverage {
 
      // assertion
      assertEquals("Enter the first number and hit enter: \nEnter the second number and hit enter: \nEnter the third number and hit enter: \nThe numbers entered are: 1, 2, and 3\nThe sum of the numbers is: 6\nThe average of the numbers is: 2.0\n", bos.toString());
-
      // undo the binding in System
      System.setOut(originalOut);
    System.setIn(originalIn);
